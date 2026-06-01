7d168655da06e95fb228e451f7d7f83c7f8b4773
Joao <joaovictorvolpato@gmail.com>
2026-06-01 09:20:53 -0300

    MudançasLogin

--- template/app.py
+++ template/app.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask(__name__)
+# CORRIGIDO: Agora com dois underlines de cada lado de name
+app = Flask(_name_)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+# 1. Rota da sua página de login atual
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        usuario = request.form.get('username')
+        senha = request.form.get('password')
+        
+        if usuario == "admin" and senha == "1234":
+            return redirect(url_for('painel'))
+        else:
+            return "Usuário ou senha incorretos!"
+            
+    return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+# 2. Rota para a NOVA PÁGINA
+@app.route('/INICIAL')
+def painel():
+    return render_template('INICIAL.html')
+
+# CORRIGIDO: Certifique-se de que a última linha também tem dois underlines em name e main!
+if _name_ == '_main_':
+    app.run(debug=True,)
